101bb5c8e6fafbfd33b6d4d6be3a670b8159a33e
Drumil Patel <42701709+drumilpatel2000@users.noreply.github.com>
2019-01-31 19:21:26 +0530

    form validation complete

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-"use strict";
+"use strict"
 
-console.log("Script is connected");
+console.log("Script is connected")
 
-var IMGForm = document.forms["IMGForm"];
+var IMGForm = document.forms["IMGForm"]
 
 function validateForm() {
-  if (IMGForm["password"].value !== IMGForm["confirmPassword"].value) {
-    alert("Password and confirm Password should be same");
-    return false;
-  }
-  var phoneREGEX = /^(91|0)?[9768]\d{9}$/;
-  console.log(honeREGEX.test(IMGForm["phoneNumber"].value));
-  if (phoneREGEX.test(IMGForm["phoneNumber"].value)) {
-    alert("Phone number is not corect");
-    return false;
-  }
+    var check = true
+    var nameREGEX = /^[a-zA-Z]+$/
+    if (!nameREGEX.test(IMGForm["name"].value)) {
+        document.getElementById("nameError").innerHTML =
+            "Name should contain only character not digits"
+    } else {
+        document.getElementById("nameError").innerHTML = ""
+    }
+    if (IMGForm["password"].value !== IMGForm["confirmPassword"].value) {
+        document.getElementById("confirmError").innerHTML =
+            "Password and confirm Password should be same"
+        check = false
+    } else {
+        document.getElementById("confirmError").innerHTML = ""
+    }
+    var phoneREGEX = /^(91|0)?[9768]\d{9}$/
+    console.log(phoneREGEX.test(IMGForm["phoneNumber"].value))
+    if (!phoneREGEX.test(IMGForm["phoneNumber"].value)) {
+        document.getElementById("phoneError").innerHTML =
+            "Phone number is not corect"
+        check = false
+    } else {
+        document.getElementById("phoneError").innerHTML = ""
+    }
+    var emailREGEX = /iitr\.ac\.in/
+    if (!emailREGEX.test(IMGForm["email"].value)) {
+        document.getElementById("emailError").innerHTML =
+            "IITR email should be used"
+        check = false
+    } else {
+        document.getElementById("emailError").innerHTML = ""
+    }
+    return check
 }
